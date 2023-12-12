b0640b23c1d8fb0b93ed440a99c0d32c3a6902e9
sohan380 <sohanmupparaju@gmail.com>
2023-12-12 23:47:02 +0530

    path adjusted

--- PDFcompressor.py
+++ PDFcompressor.py
@@ -1,22 +1,22 @@
 import PyPDF2
 import os
 
-# Define the folder path
-folder_path = "Input"
 
-# Get all files in the Input folder
-files = os.listdir("Input")
-
-for filename in files:
-    if filename.endswith(".pdf"):
-        full_filename = os.path.join(folder_path, filename)
-
-        reader = PyPDF2.PdfReader(full_filename)
-        writer = PyPDF2.PdfWriter()
-
-        for page in reader.pages:
-            page.compress_content_streams()
-            writer.add_page(page)
+def comp_pdfs(folder_path):
+    # Get all files in the Input folder
+    files = os.listdir(folder_path)
+    for filename in files:
+        if filename.endswith(".pdf"):
+            full_filename = os.path.join(folder_path, filename)
             
-        with open(os.path.join(folder_path, "compressed_" + filename), "wb") as f:
-            writer.write(f)
+            reader = PyPDF2.PdfReader(full_filename)
+            writer = PyPDF2.PdfWriter()
+            
+            for page in reader.pages:
+                page.compress_content_streams()
+                writer.add_page(page)
+            
+            with open(os.path.join(folder_path, "compressed_" + filename), "wb") as f:
+                writer.write(f)
+
+comp_pdfs("Input")
--- PDFmerger.py
+++ PDFmerger.py
@@ -1,15 +1,3 @@
-# import PyPDF2
-# import os
-
-# merger = PyPDF2.PdfMerger()
-# files = os.listdir("Input")
-
-# for file in os.listdir(os.curdir):
-#     if file.endswith(".pdf"):
-#         merger.append(file)
-
-# merger.write("combined.pdf")
-
 import PyPDF2
 import os
 
